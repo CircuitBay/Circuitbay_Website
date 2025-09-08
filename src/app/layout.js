2a17de9f4ca3f61002a64856aadbe2038c5c4370
Milan Sony <milansonyofficial@gmail.com>
2025-09-08 17:44:12 +0530

    style: fonts added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,51 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const openSans = localFont({
+  src: [
+    {
+      path: "/fonts/OpenSans-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "/fonts/OpenSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/OpenSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/OpenSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "/fonts/OpenSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "/fonts/OpenSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata = {
   title: "CircuitBay",
-  description: "CircuitBay is an e-commerce marketplace for IoT modules, sensors, microcontrollers, robotics parts, development boards, and electronics accessories.",
+  description:
+    "CircuitBay is an e-commerce marketplace for IoT modules, sensors, microcontrollers, robotics parts, development boards, and electronics accessories.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={openSans.className}>{children}</body>
     </html>
   );
 }
