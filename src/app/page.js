ab67ecc9a5f2bcc3cd1e386233691aea27aeab7a
Milan Sony <milansonyofficial@gmail.com>
2025-09-19 17:31:43 +0530

    chore: preloader removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,8 @@
-"use client"
-
-import { useEffect, useState } from "react";
 import Index from "./index/page";
-import PreLoader from "@/components/PreLoader";
 
 export default function Home() {
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      setIsLoading(false);
-    });
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
-    <>
-      {isLoading ? <PreLoader /> : <Index />}
-    </>
+    <Index />
   );
 }
