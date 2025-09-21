e85c6a37d2a72ce257777c114d55ca0018ff005f
Milan Sony <milansonyofficial@gmail.com>
2025-09-21 23:30:10 +0530

    chore: smooth scroll added, preloader alt changed

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -5,7 +5,7 @@ export default function PreLoader() {
         <div className="h-dvh w-full bg-white flex justify-center items-center text-center">
             <Image
                 src="/images/Icon_ blue.png"
-                alt="Logo"
+                alt="CircuitBay"
                 width={60}
                 height={60}
                 className="animate-pulse"
