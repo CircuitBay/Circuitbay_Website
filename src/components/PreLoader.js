0bbedce5bdd813f01254eaae74781c0bdf7755a7
Milan Sony <milansonyofficial@gmail.com>
2025-09-21 23:26:44 +0530

    style: preloader img changed

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,9 +1,15 @@
-import { LoaderPinwheel } from "lucide-react";
+import Image from "next/image";
 
 export default function PreLoader() {
     return (
-        <div className="h-dvh w-full bg-white flex justify-center items-center text-center text-blue-color">
-            <LoaderPinwheel size={48} className="animate-spin" />
+        <div className="h-dvh w-full bg-white flex justify-center items-center text-center">
+            <Image
+                src="/images/Icon_ blue.png"
+                alt="Logo"
+                width={60}
+                height={60}
+                className="animate-pulse"
+            />
         </div>
     );
 }
