6eab9bdc30108027bd4b7a7eb0bdc528ca7734ff
Milan Sony <milansonyofficial@gmail.com>
2025-09-21 23:02:37 +0530

    feat: product image url issue fixed and configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [new URL('https://raw.githubusercontent.com/CircuitBay/Product_Images/refs/heads/master/product_images/*')],
+    },
+    // Method 2
+    // images: {
+    //     domains: ["raw.githubusercontent.com"],
+    // },
+};
+
 
 export default nextConfig;
